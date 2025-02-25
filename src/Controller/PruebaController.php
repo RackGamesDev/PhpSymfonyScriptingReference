39e7ae184f8ffc5de1da1b3e7b3ef5301f9712bb
RackGamesDev <yesterdiscord7@gmail.com>
2025-02-26 00:35:28 +0100

    0

---
+++ src/Controller/PruebaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PruebaController extends AbstractController {
+    #[Route('/')] //La ruta a la que afectara este controller
+    public function index(): Response {
+        //El codigo que se ejecutara al acceder a la ruta
+        $contents = $this->renderView('home/index.html.twig'); //Leyendo un html gracias a twigs
+        echo "asdf";
+        return new Response($contents); //Siempre hay que devolver una respuesta
+    }
+}
